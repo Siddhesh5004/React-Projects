97366728ac7a50873ee1a2a058fd443b0a1013a0
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-07-11 00:32:08 +0530

    updated

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Sidebar from './components/Sidebar/Sidebar'
+
+const App = () => {
+  return (
+    <>
+    <Sidebar/>
+      </>
+  )
+}
+
+export default App
