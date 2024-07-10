597161d4c62beda51eed50dbffb60f5670d2e9f1
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-07-11 02:54:47 +0530

    updated Context.jsx

--- Gemini-Clone/src/components/Sidebar/Main/Main.jsx
+++ Gemini-Clone/src/components/Sidebar/Main/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Main.css';
-import { assets } from '../../../assets';
+import { assets } from '../../../assets/assets'
 
 const Main = () => {
   return (
@@ -32,9 +32,22 @@ const Main = () => {
             <img src={assets.code_icon} alt="code icon" />
           </div>
         </div>
+        <div className="main-bottom">
+          <div className="search-box">
+            <input type="text" placeholder='Enter a prompt here' />
+            <div>
+              <img src= {assets.gallery_icon} alt="" />
+              <img src= {assets.mic_icon} alt="" />
+              <img src= {assets.send_icon} alt="" />
+            </div>
+          </div>
+          <p className="bottom-info">
+          Gemini may display inaccurate info, including about people, so double-check its responses. Your privacy and Gemini Apps
+          </p>
+        </div>
       </div>
     </div>
   );
 };
-import './Main.css';
+
 export default Main;
