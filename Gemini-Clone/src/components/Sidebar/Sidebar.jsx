8d84ca1e7549d7a7f9cc7e1f977ae48128df115f
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-07-12 02:16:08 +0530

    updated Sidebar.jsx

--- Gemini-Clone/src/components/Sidebar/Sidebar.jsx
+++ Gemini-Clone/src/components/Sidebar/Sidebar.jsx
@@ -1,27 +1,41 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Sidebar.css'
 import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
 
 const Sidebar = () => {
 
   const [extended, setExtended] = useState(false) //if false null
+  const{onSent,prevPrompts,setRecentPrompt}=useContext(Context)
+
+  const loadPrompt = async(prompt)=>{
+    setRecentPrompt(prompt)
+    await onSent(prompt)
+  }
+
   return (
     <div className='sidebar'>
       <div className="top">
         <img  onClick ={()=>setExtended(prev=>!prev) } className='menu' src={assets.menu_icon} alt="" />
+        
 
         <div className="new-chat">
           <img src={assets.plus_icon} alt="" />
           {extended ? <p>New Chat</p> : null} 
         </div>
         {extended
-          ? <div className="recent">
+          ? <div onClick={() => loadPrompt (item)} className="recent">
             <p className="recent-title">Recent</p>
-            <div className="recent-entry">
+            {prevPrompts.map((item,index)=>{
+              return(
+                <div className="recent-entry">
               <img src={assets.message_icon} alt="" />
-              <p>What is react ...</p>
+              <p>{item.slice(0,18)}...</p>
             </div>
+              )
+            })}
+            
           </div>
           : null
         }
--- Gemini-Clone/src/context/Context.jsx
+++ Gemini-Clone/src/context/Context.jsx
@@ -4,19 +4,19 @@ import run from "../config/gemini";
 export const Context = createContext();
 const ContextProvider = (props) => {
 
-    const[input,setInput] =useState("");
-    const[recentPrompt,setRecentPrompt]=useState("")
-    const[prevPrompts,setPrevPrompts]=useState([]);
-    const[showResult,setShowResult]=useState(false);
-    const[loading,setLoading]=useState(false);
-    const[resultData,setResultData]=useState("")
+    const [input, setInput] = useState("");
+    const [recentPrompt, setRecentPrompt] = useState("")
+    const [prevPrompts, setPrevPrompts] = useState([]);
+    const [showResult, setShowResult] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const [resultData, setResultData] = useState("")
 
-    const delayPara = function (index,nextWord) {
-        setTimeout(function (){
-            setResultData(prev=>prev+nextWord)
+    const delayPara = function (index, nextWord) {
+        setTimeout(function () {
+            setResultData(prev => prev + nextWord)
 
-        },75*index)
-        
+        }, 75 * index)
+
     }
 
     const onSent = async (prompt) => {
@@ -24,31 +24,40 @@ const ContextProvider = (props) => {
         setResultData("")//reset result data
         setLoading(true) // show loading
         setShowResult(true) //show result
-        setRecentPrompt(input)
-       const response = await run(input)
-       let responseArray = response.split("**");
-       let newResponse ;
-       for(let i = 0 ; i<responseArray.length;i++)
-       {
-        if (i===0 || i%2 !==1){
-            newResponse += responseArray[i]
+        
+        let response;
+        if (prompt !== undefined) {
+            response = await run(prompt)
+            setRecentPrompt(prompt)
+        } else {
+            setPrevPrompts(prev => [...prev, input])
+            setRecentPrompt(input)
+            response = await run(input)
         }
-        else{
-            newResponse += "<b>" +responseArray[i]+"</b>";
-        }
-       }
-       let newResponse2 = newResponse.split("*").join("</br>")
-       let newResponseArray =newResponse2.split (" ");
 
-       for(let i = 0;i<newResponseArray.length;i++){
-        const nextWord=newResponseArray[i];
-        delayPara(i,nextWord +" ")
-       }
-       setLoading(false) // hide loading animation
-       setInput("")//reset input
-       
+
+        let responseArray = response.split("**");
+        let newResponse = " ";
+        for (let i = 0; i < responseArray.length; i++) {
+            if (i === 0 || i % 2 !== 1) {
+                newResponse += responseArray[i]
+            }
+            else {
+                newResponse += "<b>" + responseArray[i] + "</b>";
+            }
+        }
+        let newResponse2 = newResponse.split("*").join("</br>")
+        let newResponseArray = newResponse2.split(" ");
+
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i, nextWord + " ")
+        }
+        setLoading(false) // hide loading animation
+        setInput("")//reset input
+
     }
-   
+
     const contextValue = {
         prevPrompts,
         setPrevPrompts,
