1a175604e16e226323f5460dfa617e17327ba29a
Siddhesh Bhelke <139620000+Siddhesh5004@users.noreply.github.com>
2024-09-15 19:14:11 +0530

    deleted Cards

--- Gemini-Enhanced Chatbot/src/components/Main/Main.jsx
+++ Gemini-Enhanced Chatbot/src/components/Main/Main.jsx
@@ -19,24 +19,6 @@ const Main = () => {
           <p><span>Hello, User</span></p>
           <p>How can I help you today?</p>
         </div>
-        <div className="cards">
-          <div className="card">
-            <p>Suggest beautiful places on an upcoming road trip</p>
-            <img src={assets.compass_icon} alt="compass icon" />
-          </div>
-          <div className="card">
-            <p>Briefly Summarize this concept : urban planning</p>
-            <img src={assets.bulb_icon} alt="bulb icon" />
-          </div>
-          <div className="card">
-            <p>Brainstorm team bonding activities for our work retreat</p>
-            <img src={assets.message_icon} alt="message icon" />
-          </div>
-          <div className="card">
-            <p>Improve the readability of the following code</p>
-            <img src={assets.code_icon} alt="code icon" />
-          </div>
-        </div>
         </>
         :<div className='result'>
             <div className="result-title">
